46eba81b71143e7ef6c5f2324def13df5d93b834
CarltonNcube <carltonncube@outlook.com>
2024-03-21 21:08:49 +0000

    List all bands with Glam rock as their main style, ranked by their longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- List all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name,
+    CASE
+        WHEN split IS NOT NULL THEN 2022 - CAST(split AS UNSIGNED)
+        ELSE 2022 - CAST(formed AS UNSIGNED)
+    END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC, band_name;
