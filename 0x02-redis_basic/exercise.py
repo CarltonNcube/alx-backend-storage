be82ddfbcfbcc248a611478cdeb1ee4eaaf33794
Carlton Ncube <carltonncube@outlook.com>
2024-03-27 20:57:53 +0200

    a store method that takes a data argument and returns a string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -85,47 +85,44 @@ class Cache:
         return int(data)
 
     def count_calls(method: Callable) -> Callable:
+    """ Decorator for Cache class methods to track call count
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """ Wraps called method and adds its call count redis before execution
         """
-        Decorator for Cache class methods to track call count
-        """
-        @wraps(method)
-        def wrapper(self: Any, *args, **kwargs) -> str:
-            """
-            Wraps called method and adds its call count redis before execution
-            """
-            self._redis.incr(method.__qualname__)
-            return method(self, *args, **kwargs)
-        return wrapper
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
-    def call_history(method: Callable) -> Callable:
-        """
-        Decorator for Cache class method to track args
-        """
-        @wraps(method)
-        def wrapper(self: Any, *args) -> str:
-            """
-            Wraps called method and tracks its passed argument by storing
+
+def call_history(method: Callable) -> Callable:
+    """ Decorator for Cache class method to track args
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args) -> str:
+        """ Wraps called method and tracks its passed argument by storing
             them to redis
-            """
-            self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
-            output = method(self, *args)
-            self._redis.rpush(f'{method.__qualname__}:outputs', output)
-            return output
-        return wrapper
-
-    def replay(fn: Callable) -> None:
         """
-        Check redis for how many times a function was called and display:
+        self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
+        output = method(self, *args)
+        self._redis.rpush(f'{method.__qualname__}:outputs', output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable) -> None:
+    """ Check redis for how many times a function was called and display:
             - How many times it was called
             - Function args and output for each call
-        """
-        client = redis.Redis()
-        calls = client.get(fn.__qualname__).decode('utf-8')
-        inputs = [input.decode('utf-8') for input in
-                  client.lrange(f'{fn.__qualname__}:inputs', 0, -1)]
-        outputs = [output.decode('utf-8') for output in
-                   client.lrange(f'{fn.__qualname__}:outputs', 0, -1)]
-        print(f'{fn.__qualname__} was called {calls} times:')
-        for input, output in zip(inputs, outputs):
-            print(f'{fn.__qualname__}(*{input}) -> {output}')
+    """
+    client = redis.Redis()
+    calls = client.get(fn.__qualname__).decode('utf-8')
+    inputs = [input.decode('utf-8') for input in
+              client.lrange(f'{fn.__qualname__}:inputs', 0, -1)]
+    outputs = [output.decode('utf-8') for output in
+               client.lrange(f'{fn.__qualname__}:outputs', 0, -1)]
+    print(f'{fn.__qualname__} was called {calls} times:')
+    for input, output in zip(inputs, outputs):
+        print(f'{fn.__qualname__}(*{input}) -> {output}')
 
