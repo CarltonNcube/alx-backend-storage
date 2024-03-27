af81695e84905f1859fd29b27fde71f2a53af899
Carlton Ncube <carltonncube@outlook.com>
2024-03-27 23:08:05 +0200

    a store method that takes a data argument and returns a string

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
 
 """
-This script defines a function to get the HTML content of a URL while
-tracking the number of times the URL was accessed and caching the result.
+This script defines a function to get the HTML content.
 """
 
-import requests
 import redis
-import time
+import requests
 from functools import wraps
+from typing import Callable
 
-# Initialize Redis connection
+
 redis_conn = redis.Redis()
 
 def cache_page(func):
+    """
+    Decorator for caching page content and tracking access count
+    """
     @wraps(func)
     def wrapper(url):
-        # Check if URL access count is cached
-        count_key = f"count:{url}"
-        access_count = redis_conn.get(count_key)
-        
-        if access_count:
-            access_count = int(access_count)
-        else:
-            access_count = 0
-
-        # Increment URL access count and cache with expiration time of 10 seconds
-        access_count += 1
-        redis_conn.setex(count_key, 10, access_count)
+        """
+        Wrapper that:
+        - Checks whether a URL's data is cached
+        - Tracks how many times get_page is called
+        """
+        # Increment URL access count
+        redis_conn.incr(f'count:{url}')
 
         # Check if page content is cached
         page_content = redis_conn.get(url)
@@ -44,5 +41,9 @@ def cache_page(func):
 
 @cache_page
 def get_page(url: str) -> str:
+    """
+    Makes an HTTP request to a given endpoint
+    """
     response = requests.get(url)
     return response.text
+
